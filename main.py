57d2f8237df8e3450dc5df0cf0196a2992c6763e
Ubuntu <ubuntu@ip-10-129-30-176.ec2.internal>
2020-04-20 14:21:19 +0000

    test rolling update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World!, Thia ia Bob"
+    return "Hello, World!, This is student Bob in Udacity Cloud DevOps"
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
