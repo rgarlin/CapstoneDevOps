9038873b492d79dcb83d2068a6f2065f024f7a9c
Ubuntu <ubuntu@ip-10-129-30-176.ec2.internal>
2020-04-23 15:05:02 +0000

    changed readme

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World!, This is Bob"
+    return "Hello, World!, This is student Bob"
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
